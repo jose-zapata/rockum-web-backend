4ed3348d03c8a72e0d8ef95800106c68d1627eec
Jose Zapata <jose.zapata@gmail.com>
2024-08-15 19:45:43 -0500

    Cambio de portada de acuerdo a especificaciones del cliente

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,9 +3,9 @@
     <x-section class="mb-4 main">
       <x-row class="flex-wrap lg:flex-nowrap">
         <x-col class="main-content">
-          <h2 class="text-4xl mt-3 mb-2 font-bebas-neue uppercase font-semibold">Latest news</h2>
           <x-row class="my-2 flex-wrap content-baseline">
             <x-col class="basis-1/4">
+              <h2 class="text-3xl mb-2 font-bebas-neue font-semibold">Latest news</h2>
               @foreach ($reports->take(3) as $item)
                 <x-story
                   title="{{ Str::limit($item->titulo, 50, '...') }}"
@@ -16,12 +16,14 @@
               @endforeach
             </x-col>
             <x-col class="basis-1/2">
+              <h2 class="text-3xl mb-2 font-bebas-neue font-semibold">&nbsp;</h2>
               @foreach ($reports->skip(3)->take(1) as $item)
-                <img src="{{ $item->imagen }}" class="block" />
-                <h2 class="text-2xl font-bebas-neue p-2 text-white font-semibold bg-neutral-500"><a href="{{ $item->url }}">{!! $item->titulo !!}</a></h2>
+                <a href="{{ $item->url }}"><img src="{{ $item->imagen }}" class="block" /></a>
+                <h2 class="text-2xl font-bebas-neue p-2 text-white font-semibold bg-neutral-500"><a href="{{ $item->url }}" class="block">{!! $item->titulo !!}</a></h2>
               @endforeach
             </x-col>
             <x-col class="basis-1/4">
+              <h2 class="text-3xl mb-2 font-bebas-neue font-semibold">Rockum radio</h2>
               @foreach ($reports->skip(4)->take(3) as $item)
                 <x-story
                   title="{{ Str::limit($item->titulo, 50, '...') }}"
